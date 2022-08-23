67ff85a671c6a68035627248f637b13f2ce9f2b7
David Szepesvari <dsz@google.com>
2022-08-23 15:54:23 -0700

    Test that checks get_state and set_state leads to reproducible steps.
    PiperOrigin-RevId: 469577304
    Change-Id: I165aee60be6a4576e59f4414d7ff30255d0cd7fc

--- csuite/csuite_test.py
+++ csuite/csuite_test.py
@@ -15,10 +15,14 @@
 
 """Tests all environments through the csuite interface."""
 
+import typing
+
 from absl.testing import absltest
 from absl.testing import parameterized
 
 import csuite
+from dm_env import specs
+import numpy as np
 
 
 class CSuiteTest(parameterized.TestCase):
@@ -43,6 +47,57 @@ class CSuiteTest(parameterized.TestCase):
         obs, unused_reward = env.step(action_spec.generate_value())
 
     env.set_state(init_state)
+
+  @parameterized.parameters([e.value for e in csuite.EnvName])
+  def test_env_state_resets(self, env_name):
+    """Tests that `get`ing and `set`ing state results in reproducibility."""
+    # Since each environment is different, we employ a generic strategy that
+    # should
+    #
+    # a) get us to a variety of states to query the state on,
+    # b) take a number of steps from that state to check reproducibility.
+    #
+    # See the implementation for the specific strategy taken.
+    num_steps_to_check = 4
+    env = csuite.load(env_name)
+    env.start()
+    action_spec = env.action_spec()
+
+    if not isinstance(action_spec, specs.DiscreteArray):
+      raise NotImplementedError(
+          "This test only supports environments with discrete action "
+          "spaces for now. Please raise an issue if you want to work with a "
+          "a non-discrete action space.")
+
+    action_spec = typing.cast(specs.DiscreteArray, action_spec)
+    for action in range(action_spec.num_values):
+      env.step(action)
+      orig_state = env.get_state()
+      outputs_1 = [env.step(action) for _ in range(num_steps_to_check)]
+      observations_1, rewards_1 = zip(*outputs_1)
+      env.set_state(orig_state)
+      outputs_2 = [env.step(action) for _ in range(num_steps_to_check)]
+      observations_2, rewards_2 = zip(*outputs_2)
+      with self.subTest("observations", action=action):
+        self.assertSameObsSequence(observations_1, observations_2)
+      with self.subTest("rewards", action=action):
+        self.assertSequenceEqual(rewards_1, rewards_2)
+
+  def assertSameObsSequence(self, seq1, seq2):
+    """The observations are expected to be numpy objects."""
+    # self.assertSameStructure(seq1, seq2)
+    problems = []  # (idx, problem str)
+    for idx, (el1, el2) in enumerate(zip(seq1, seq2)):
+      try:
+        np.testing.assert_array_equal(el1, el2)
+      except AssertionError as e:
+        problems.append((idx, str(e)))
+
+    if problems:
+      self.fail(
+          f"The observation sequences (of length {len(seq1)}) are not the "
+          "same. The differences are:\n" +
+          "\n".join([f"at idx={idx}: {msg}" for idx, msg in problems]))
 
 
 if __name__ == "__main__":
